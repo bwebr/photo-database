e028eefdab8799ef9f1bedf069966bfccec95040
bwebr <weberben.r@gmail.com>
2016-09-22 19:13:51 +0100

    fixed footer, lightbox effect

--- js/index.js
+++ js/index.js
@@ -3,29 +3,30 @@ var i = 0;
 
 function changeImage() {
 	$("#currentImage").attr("src", "images/" + images[i] + ".jpg");
-	}
+}
 
 function previousImage() {
-	i--;
+	i = i - 1; /* you're reassigning i to be the current value of i - 1 */
 	changeImage();
 
-  if (i <=0) {
-    i = images.length -1;
+  if (i === 0) { /* if first image then reset i */
+    i = images.length;
   }
 }
 
 function nextImage() {
-	i++;
+	i = i + 1; /* you're reassigning i to be the current value of i + 1 */
 	changeImage();
 
-  if (i <= images.length -1) {
-    i = 0;
+  if (i === images.length - 1) { /* if last image then reset i */
+    i = -1;
   }
 }
 
 function goArchive() {
 // ???????????????
 }
+
 
 $("#forward").click(nextImage);
 $("#back").click(previousImage);
